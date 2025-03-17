b60a4043f1f87e8f0ba1f3693c92aea433fb3856
FajarBudiawan <you@example.com>
2025-03-18 01:50:49 +0700

    commit modul 1

---
+++ MODUL 1/identitas.js
@@ -0,0 +1,21 @@
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+input.question("siapa nama kamu?", (nama) => {
+  console.info(`Hallo ${nama}`);
+  input.question("jurusan kamu?", (jurusan) => {
+    console.info(`Hallo ${jurusan}`);
+    input.question("dari univ mana kamu?", (universitas) => {
+      console.info(`Hallo ${universitas}`);
+      input.question("umur?", (umur) => {
+        console.info(` ${umur}`);
+        input.close();
+      });
+    });
+  });
+});
